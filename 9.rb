e0ebfa49cced62a1a6e8395270448149ae70df0c
kuro <kuro@wlan2205.u-aizu.ac.jp>
2015-11-26 17:20:21 +0900

    変更

--- 9.rb
+++ 9.rb
@@ -1,3 +1,13 @@
 n=STDIN.gets
 arr=STDIN.gets.split.map(&:to_i)
-puts "#{arr.min} #{arr.max} #{arr.inject(:+)}"
+arr2 = Array[]
+arr2[0] = arr.inject(-1000000) do |a, i|
+  a= a > i ? a:i
+end
+
+arr2[1] = arr.inject(1000000) do |a, i|
+  a= a < i ? a:i
+end
+arr2[2] =arr.inject(:+)
+
+print arr2
