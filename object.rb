f95a8c3310435ac7251cef066457d337dd394edf
kuro <kuro@KURO-no-MacBook-Pro.local>
2015-12-18 00:42:58 +0900

    family_populationの作成

--- object.rb
+++ object.rb
@@ -1,12 +1,33 @@
 class Human
+  
+  @@population = 0
+  @@family_population = {}
+  
   def initialize(firstName, middleName=nil, familyName, age)
     @name = Name.new(firstName, middleName, familyName)
     @age = age
+    @@population+=1
+    
+    if @@family_population[familyName].nil? then
+      @@family_population[familyName] = 1
+    else
+      @@family_population[familyName]+=1
+    end
   end
+  
   
   def to_s
     "#{@name} (#{@age})"
   end
+
+  def self.family_population(familyName)
+    if @@family_population[familyName].nil? then
+      0
+    else
+      @@family_population[familyName]
+    end
+  end
+  
 end
 
 class Name
@@ -25,8 +46,13 @@ class Name
   end
 end
 
-puts Name.new('Barack', 'Obama')
-puts Name.new('George', 'W', 'Bush')
+Human.new('Willard', 'Smith', 47)
+Human.new('Shelley', 'Smith', 43)
+Human.new('Frank', 'Williams', 63)
+Human.new('Scott', 'Brown', 32)
+Human.new('Jane', 'Brown', 25)
+Human.new('Bobby', 'Brown', 22)
 
-puts Human.new('Barack', 'Obama', 54)
-puts Human.new('George', 'W', 'Bush', 69)
+puts Human.family_population('Smith')
+puts Human.family_population('Williams')
+puts Human.family_population('Brown')
